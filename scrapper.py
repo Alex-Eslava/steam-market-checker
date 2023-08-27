3b188509a7db6deb9d64e8db902eaca1e2b3ff6c
Alex-Eslava <alex.eslava96@gmail.com>
2023-08-27 11:56:45 +0200

    linter
    smallfix
    
    reducing scope
    
    updating gitignore for clearer shellprompts
    
    improved error logging
    
    patient retries
    
    lenient sleep
    
    norender true kinda sexy
    
    smallfix
    
    betterment of variables
    
    colsfix
    
    eerie
    
    uncatchable error retries added
    
    less verbose
    
    improvements for rate limiters
    
    logging
    
    simplyfing things
    
    errata fix
    
    loose matching fix

--- scrapper.py
+++ scrapper.py
@@ -1,53 +1,58 @@
 import os
 import pandas as pd
 from utils import market_search
-from sm_constants import VFX_DICT
+from sm_constants import VFX_DICT, UNUS_COLS
 
 
-def get_search_results(vfx, appid=440, count=50):
+def get_search_results(vfx, appid=440, count=100):
     # TF2's appid is 440
     params = {
-        'query': vfx,
-        'appid': appid,
-        'count': count,
-        'search_descriptions': 1,
-        'category_440_Quality[]': 'tag_rarity4',
+        "query": vfx,
+        "appid": appid,
+        "count": count,
+        "search_descriptions": 1,
+        "category_440_Quality[]": "tag_rarity4",
+        "norender": 1,  # No need for beautifulsoups
     }
-    return market_search(params) or [] 
+    return market_search(params) or []
 
 
 def fetch_previous_data(file_path):
     try:
-        return pd.read_csv(file_path, sep='|')
+        return pd.read_csv(file_path, sep="|")
     except FileNotFoundError:
+        print("No market history db fount. Creating one...")
         return pd.DataFrame()
 
 
 def detect_market_changes(current_df, previous_df):
-    # Merge based on the specific columns
-    merged_df = current_df.merge(previous_df, on=['unusual_effect', 'item_name', 'sale_price'], indicator=True, how='outer')
-    # Filter to only get rows present in `current_df` but not in `previous_df`
-    new_entries = merged_df[merged_df['_merge'] == 'left_only']
-    # Drop the _merge column before returning
-    return new_entries.drop(columns=['_merge'])
-
+    if previous_df is None or previous_df.empty:
+        return current_df
+    # We noticed small FX change differences on a daily basis so we will ignore +-1.99 usd differences
+    current_df["truncated_price"] = current_df["sale_price_text"].str[:-4]
+    previous_df["truncated_price"] = previous_df["sale_price_text"].str[:-4]
+    filtered_df = current_df[
+        ~current_df.set_index(["name", "truncated_price"]).index.isin(
+            previous_df.set_index(["name", "truncated_price"]).index
+        )
+    ].drop(columns=["truncated_price"])
+    return filtered_df
 
 
 def main():
     for vfx_key, vfx_values in VFX_DICT.items():
-        vfx_results_list = [result for vfx in vfx_values for result in get_search_results(vfx)]
-        
-        current_df = pd.DataFrame(vfx_results_list)
+        vfx_results_list = [
+            result for vfx in vfx_values for result in get_search_results(vfx)
+        ]
         file_path = os.path.join("data", f"{vfx_key}_df.csv")
-        
+        current_df = pd.DataFrame(vfx_results_list)
         previous_df = fetch_previous_data(file_path)
-        
         diff_df = detect_market_changes(current_df, previous_df)
-        current_df.to_csv(file_path, sep='|', index=False)
+        current_df[UNUS_COLS].to_csv(file_path, sep="|", index=False)
 
         if not diff_df.empty:
             print(f"[X] - For {vfx_key} we detected market changes:")
-            print(diff_df)
+            print(diff_df[["unusual_effect", "name", "sale_price_text"]])
         else:
             print(f"[_] No market changes for {vfx_key}")
     return True
--- utils.py
+++ utils.py
@@ -3,13 +3,8 @@ import json
 import time
 from bs4 import BeautifulSoup
 
+
 def query_steam_market(params):
-    """
-    Query the Steam Community Market for a particular item.
-    
-    :param item_name: Name of the item to search for.
-    :return: JSON response of the query.
-    """
     # Define the Steam Market search URL
     base_url = "https://steamcommunity.com/market/search/render/"
     response = requests.get(base_url, params=params)
@@ -20,48 +15,60 @@ def query_steam_market(params):
     # Convert the response text to JSON
     return json.loads(response.text)
 
+
 def parse_market_results_html(results_html, params):
-    soup = BeautifulSoup(results_html, 'html.parser')
+    soup = BeautifulSoup(results_html, "html.parser")
     # Find all listing links, which will contain the desired data
-    listings = soup.find_all('a', class_='market_listing_row_link')
+    listings = soup.find_all("a", class_="market_listing_row_link")
     parsed_data = []
     for listing in listings:
-        data = {'unusual_effect':params['query']}
+        data = {"unusual_effect": params["query"]}
         # Extracting the item name
-        item_name_element = listing.find('span', class_='market_listing_item_name')
+        item_name_element = listing.find("span", class_="market_listing_item_name")
         if item_name_element:
-            data['item_name'] = item_name_element.text
+            data["item_name"] = item_name_element.text
         # Extracting the game name
-        game_name_element = listing.find('span', class_='market_listing_game_name')
+        game_name_element = listing.find("span", class_="market_listing_game_name")
         if game_name_element:
-            data['game_name'] = game_name_element.text
+            data["game_name"] = game_name_element.text
         # Extracting the item image link
-        item_img_element = listing.find('img', class_='market_listing_item_img')
-        if item_img_element and 'src' in item_img_element.attrs:
-            data['item_img'] = item_img_element['src'] 
+        item_img_element = listing.find("img", class_="market_listing_item_img")
+        if item_img_element and "src" in item_img_element.attrs:
+            data["item_img"] = item_img_element["src"]
         # Extracting the item price details
-        price_element = listing.find('span', class_='normal_price')
+        price_element = listing.find("span", class_="normal_price")
         if price_element:
-            data['normal_price'] = price_element.text.strip()
-        sale_price_element = listing.find('span', class_='sale_price')
+            data["normal_price"] = price_element.text.strip()
+        sale_price_element = listing.find("span", class_="sale_price")
         if sale_price_element:
-            data['sale_price'] = sale_price_element.text.strip() 
+            data["sale_price"] = sale_price_element.text.strip()
         # Extracting the quantity available
-        qty_element = listing.find('span', class_='market_listing_num_listings_qty')
+        qty_element = listing.find("span", class_="market_listing_num_listings_qty")
         if qty_element:
-            data['quantity_available'] = qty_element.text
+            data["quantity_available"] = qty_element.text
         parsed_data.append(data)
     return parsed_data
 
-def market_search(params, max_retries=5, delay=60):
+
+def market_search(params, max_retries=4, delay=300):
+    # 200 requests every 5 minutes is the rate limit
+    # Nontheless we gotta take into account uncatchable "steam error"
     for i in range(max_retries):
-        results = query_steam_market(params)
-        
-        if results:
-            return parse_market_results_html(results['results_html'], params) or []
-        
-        if results is None:  # Assuming `None` indicates a 429 error; adjust as needed.
-            print(f"Warning: Attempt {i+1} of {max_retries}. Encountered HTTP 429. Retrying in {delay} seconds...")
-            time.sleep(delay)
-    print(f"Error: Exceeded {max_retries} attempts. Skipping...")
+        response = query_steam_market(params)
+
+        if response:
+            if len(response["results"]) == 0:
+                time.sleep(1)
+            else:
+                for result in response["results"]:
+                    result["unusual_effect"] = params["query"]
+                return response[
+                    "results"
+                ]  # parse_market_results_html(results["results"], params) or []
+
+        if response is None:  # Assuming `None` indicates a 429 error; adjust as needed.
+            print(
+                f"Warning: Attempt {i+1} of {max_retries}. Encountered HTTP 429. Retrying for {params['query']} in {delay+i*30} seconds..."
+            )
+            time.sleep(delay + i * 30)
     return []  # Return an empty list instead of None
