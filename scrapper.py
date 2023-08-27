a258f9b706ba3fcb7f12cd2cce40d76ccbbe552f
Alex-Eslava <alex.eslava96@gmail.com>
2023-08-27 10:40:39 +0200

    Learned about Status Code 429

--- scrapper.py
+++ scrapper.py
@@ -3,37 +3,49 @@ import pandas as pd
 from utils import market_search
 from sm_constants import VFX_DICT
 
+
+def get_search_results(vfx, appid=440, count=50):
+    # TF2's appid is 440
+    params = {
+        'query': vfx,
+        'appid': appid,
+        'count': count,
+        'search_descriptions': 1,
+        'category_440_Quality[]': 'tag_rarity4',
+    }
+    return market_search(params)
+
+
+def fetch_previous_data(file_path):
+    try:
+        return pd.read_csv(file_path, sep='|')
+    except FileNotFoundError:
+        return pd.DataFrame()
+
+
+def detect_market_changes(current_df, previous_df):
+    return pd.concat([current_df, previous_df]).drop_duplicates(keep=False)
+
+
 def main():
-
-    for vfx_key in VFX_DICT.keys():
-        vfx_results_list = []
-        for vfx in VFX_DICT[vfx_key]:
-            params = {
-                'query': vfx,
-                'appid': 440,  # This is for TF2.
-                'count': 50,   # Number of results. You can adjust this.
-                'search_descriptions': 1,  # This will search the description as well.
-                'category_440_Quality[]': 'tag_rarity4',  # Searching Unusuals
-            }
-            vfx_results_list.extend(market_search(params))
-
-        # Transform list of dictionaries into pandas dataframe
+    for vfx_key, vfx_values in VFX_DICT.items():
+        vfx_results_list = [get_search_results(vfx) for vfx in vfx_values if get_search_results(vfx)]
+        
         current_df = pd.DataFrame(vfx_results_list)
         file_path = os.path.join("data", f"{vfx_key}_df.csv")
-        # Read previous file
-        try:
-            previous_df = pd.read_csv(file_path, sep='|')
-        except FileNotFoundError:
-            previous_df = pd.DataFrame()
-        # Compare the previous dataframe to the new one and store in another dataframe the differences
-        diff_df = pd.concat([current_df, previous_df]).drop_duplicates(keep=False)
+        
+        previous_df = fetch_previous_data(file_path)
+        
+        diff_df = detect_market_changes(current_df, previous_df)
         current_df.to_csv(file_path, sep='|', index=False)
-        if len(diff_df) > 0:
+
+        if not diff_df.empty:
             print(f"[X] - For {vfx_key} we detected market changes:")
             print(diff_df)
-        else: 
+        else:
             print(f"[_] No market changes for {vfx_key}")
     return True
+
 
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import time
 from bs4 import BeautifulSoup
 
 def query_steam_market(params):
@@ -52,6 +53,17 @@ def parse_market_results_html(results_html, params):
         parsed_data.append(data)
     return parsed_data
 
-def market_search(params):
-    results = query_steam_market(params)
-    return parse_market_results_html(results['results_html'], params) if results else None
+def market_search(params, max_retries=5, delay=60):
+    for i in range(max_retries):
+        results = query_steam_market(params)
+        
+        if results:
+            return parse_market_results_html(results['results_html'], params)
+        
+        if results is None:  # Assuming `None` indicates a 429 error; adjust as needed.
+            print(f"Warning: Attempt {i+1} of {max_retries}. Encountered HTTP 429. Retrying in {delay} seconds...")
+            time.sleep(delay)
+        else:
+            return None
+    print(f"Error: Exceeded {max_retries} attempts. Skipping...")
+    return None
